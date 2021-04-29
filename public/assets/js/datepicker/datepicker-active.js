32d2a4036babdff635ae24fc158e2679545639fe
rania <rania.sellami@esprit.com>
2021-04-29 05:26:00 +0200

    add assets

---
+++ public/assets/js/datepicker/datepicker-active.js
@@ -0,0 +1,11 @@
+(function ($) {
+    "use strict";
+
+    // Datepickers
+    $('.hasDatepicker').datepicker({
+        dateFormat: 'dd.mm.yy',
+        prevText: '<i class="fa fa-chevron-left"></i>',
+        nextText: '<i class="fa fa-chevron-right"></i>'
+    });
+
+})(jQuery); 
